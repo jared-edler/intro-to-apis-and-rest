053fbca58b4a641e7276455f506bc3ce4fec2304
Jared Edler <jared.edler@bayer.com>
2023-09-26 20:59:53 -0500

    initial setup

---
+++ resources/db/create_table.sql
@@ -0,0 +1,12 @@
+create table stl_city_players (
+    shirt_number                  integer NOT NULL,
+    first_name                    varchar NOT NULL,
+    last_name                     varchar,
+    country                       varchar NOT NULL,
+    field_position                varchar NOT NULL,
+    goals                         integer NOT NULL,
+    assists                       integer NOT NULL,
+    constraint stl_city_players_pk primary key(shirt_number)
+);
+
+commit
